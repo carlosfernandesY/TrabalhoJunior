9a84b71007ad0ef1f5a668502970be9415942f73
carlosfernandesY <ferreirafernandescarlosroberto@gmail.com>
2026-06-08 18:11:40 -0300

    Adicionei mais arestas

--- src/main/java/org/ifgoiano/Main.java
+++ src/main/java/org/ifgoiano/Main.java
@@ -4,10 +4,17 @@ void main(){
     Grafo grafo = new Grafo(12);
     grafo.adicionarAresta(0,1);
     grafo.adicionarAresta(0,4);
-    grafo.adicionarAresta(1,3);
+    grafo.adicionarAresta(1,2);
     grafo.adicionarAresta(1,5);
     grafo.adicionarAresta(2,3);
     grafo.adicionarAresta(2,6);
+    grafo.adicionarAresta(3,7);
+    grafo.adicionarAresta(4,5);
+    grafo.adicionarAresta(5,9);
+    grafo.adicionarAresta(5,6);
+    grafo.adicionarAresta(6,7);
+    grafo.adicionarAresta(6,11);
+
 
 
     grafo.mostrarGrafo();
